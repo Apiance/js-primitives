e847cbeeeba0211ef14380414a8cac7325ec8dbe
Apiance-CI <alex+apiance@subterminal-networks.com>
2020-08-08 23:16:24 +0200

    feat(Market): fromString constructor + base/quoteSymbol

--- src/Market/Market.js
+++ src/Market/Market.js
@@ -1,7 +1,21 @@
+const fromString = (value) =>{
+  const [exchange, symbol] = value.split('::');
+  return new Market({exchange, symbol});
+}
 class Market {
   constructor(props = {}) {
+    if(props.constructor === String){
+      return fromString(props);
+    }
     this.exchange = props?.exchange?.toString().toUpperCase() || null;
     this.symbol = props?.symbol?.toString().toUpperCase() || null;
+
+    if(props.quoteSymbol) {
+      this.quoteSymbol = props.quoteSymbol;
+    }
+    if(props.baseSymbol) {
+      this.baseSymbol = props.baseSymbol;
+    }
   }
   toObject(){
     return this;
