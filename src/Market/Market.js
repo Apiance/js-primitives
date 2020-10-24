0fe82674d5fd38b034a2098b17cf0abe7e7cfb4d
Apiance-CI <alex+apiance@subterminal-networks.com>
2020-10-24 22:46:09 +0200

    feat(Market): specific management of Volume and FTX market

--- src/Market/Market.js
+++ src/Market/Market.js
@@ -1,7 +1,22 @@
 const fromString = (value) =>{
-  const [exchange, symbol] = value.split('::');
-  return new Market({exchange, symbol});
+  const [exchange, market] = value.split('::');
+  const splitted = market.split('-');
+  if(splitted.length>1){
+    if(exchange === 'FTX' && splitted[1] === 'PERP'){
+      return new Market({exchange, symbol: market});
+    }
+    const [quote, base, type] = splitted.reverse();
+    return new Market({exchange, type, quote, base});
+  }else{
+    return new Market({exchange, symbol:splitted[0]});
+  }
 }
+
+/**
+ * Can be init either via Symbol or full
+ *
+ *
+ */
 class Market {
   constructor(props = {}) {
     if(props.constructor === String){
@@ -9,14 +24,10 @@ class Market {
     }
     if(!props) throw new Error('Expected props to create Market');
     this.exchange = (props.exchange) ? props.exchange.toString().toUpperCase() : null;
+    this.type = (props.type) ? props.type.toString().toUpperCase() : null;
     this.symbol = (props.symbol) ? props.symbol.toString().toUpperCase() : null;
-
-    if(props.quoteSymbol) {
-      this.quoteSymbol = props.quoteSymbol;
-    }
-    if(props.baseSymbol) {
-      this.baseSymbol = props.baseSymbol;
-    }
+    this.quote = (props.quote) ? props.quote.toString().toUpperCase() : null;
+    this.base = (props.base) ? props.base.toString().toUpperCase() : null;
   }
   toObject(){
     return this;
@@ -25,7 +36,15 @@ class Market {
     return this.toString();
   }
   toString(){
-    return `${this.exchange.toString()}::${this.symbol}`;
+    if(this.symbol && !this.type){
+      return `${this.exchange.toString()}::${this.symbol}`;
+    }else{
+      let marketId = `${this.quote}-${this.base}`;
+      if(this.type){
+        marketId = `${this.type}-${marketId}`;
+      }
+      return `${this.exchange.toString()}::${marketId}`;
+    }
   }
 };
 module.exports = Market;
