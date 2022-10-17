62f29a4179f8ffe6b50989461ab21b78e31cdc5f
Alex Werner <obusco@gmail.com>
2022-10-17 23:48:27 +0200

    fix(ZCandle): correctly keep Market when Candle>zCandle>Candle

--- src/Market/Market.js
+++ src/Market/Market.js
@@ -1,10 +1,11 @@
 const TYPES = require('./TYPES');
 const fromString = (value) =>{
-  const [exchange, market] = value.split('::');
-  const splitted = market.split('-');
+  const [exchange, symbol] = value.split('::');
+  const splitted = symbol.split('-');
   if(splitted.length>1){
+
     if(exchange === 'FTX' && splitted[1] === 'PERP'){
-      return new Market({exchange, symbol: market, type: TYPES.PERP, quote: 'USD', base: splitted[0]});
+      return new Market({exchange, symbol, quote: 'USD', base: splitted[0], type: TYPES.PERP });
     }
     const [quote, base, type] = splitted.reverse();
     return new Market({exchange, type, quote, base});
@@ -18,15 +19,17 @@ const fromString = (value) =>{
  *
  *
  */
+// FIXME: The handling of FTX // PERP is suboptimal.
+//  Refactorize me please <3
 class Market {
   constructor(props = {}) {
+    if(!props) throw new Error('Expected props to create Market');
     if(props.constructor === String){
       return fromString(props);
     }
-    if(!props) throw new Error('Expected props to create Market');
     this.exchange = (props.exchange) ? props.exchange.toString().toUpperCase() : null;
-    this.type = (props.type) ? props.type.toString().toUpperCase() : null;
     this.symbol = (props.symbol) ? props.symbol.toString().toUpperCase() : null;
+    this.type = (props.type) ? props.type.toString().toUpperCase() : null;
 
     this.quote = null;
     this.base = null;
@@ -36,6 +39,16 @@ class Market {
       this.base = (base) ? base : (props.symbol.split(quote)[0].toString().toUpperCase())
       this.quote = (quote) ? quote : (props.symbol.split(base)[1].toString().toUpperCase())
     }
+
+    if(this.exchange === 'FTX'){
+      const splitted = this.symbol.split('-');
+      if(splitted.length>1 && splitted[1]==='PERP'){
+        this.type = TYPES.PERP;
+        this.quote = 'USD';
+        this.base = splitted[0];
+      }
+    }
+
   }
   toObject(){
     const { exchange, type, symbol, quote, base } = this;
@@ -49,6 +62,11 @@ class Market {
   }
   toString(){
     let marketId = (this.base && this.quote) ? `${this.base}-${this.quote}` : this.symbol;
+
+    if(this.type === TYPES.PERP && this.exchange === 'FTX'){
+      return `${this.exchange.toString()}::${this.symbol}`
+    }
+
     return (this.type) ? `${this.exchange.toString()}::${this.type}-${marketId}` : `${this.exchange.toString()}::${marketId}`;
   }
   toCompressed(){
--- src/ZCandle/ZCandle.spec.js
+++ src/ZCandle/ZCandle.spec.js
@@ -1,95 +1,112 @@
-const { expect } = require('chai');
+const {expect} = require('chai');
 const ZCandle = require('./ZCandle');
 const Candle = require('../Candle/Candle');
 
-describe('ZCandle', function suite(){
-  const zippedCandleStr1 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100';
-  const zippedCandleStr2 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::42';
-  const zippedCandleStr3 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::-151500::42';
-  const zippedCandleStr4 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-151500::42';
-  const zippedCandleStr5 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-::42';
-  const zippedCandleStr6 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-';
-  const opts = {
-    exchange: 'KRAKEN',
-    symbol: 'BTCUSD',
-    interval: '1d',
-    timestamp: '2020-08-02T00:00:00.000Z',
-    open:'10000',
-    high:'10111',
-    low: '10000',
-    close: '10100',
-  };
-  const candle1 = new Candle(opts);
-  const candle2 = new Candle({...opts, trades: "42"});
-  const candle3 = new Candle({...opts, trades: "42", volume: {quote: "151500"}});
-  const candle4 = new Candle({...opts, trades: "42", volume: {quote: "151500", base: "15"}});
-  const candle5 = new Candle({...opts, trades: "42", volume: { base: "15"}});
-  const candle6 = new Candle({...opts, volume: { base: "15"}});
-  it('should instantiates', ()=>{
-    zcandle = new ZCandle();
-    expect(zcandle).to.exist;
-  })
-  it('should init from candle', function () {
-    const zcandle1 = new ZCandle(candle1);
-    expect(zcandle1.toString()).to.equal(zippedCandleStr1);
-    const zcandle2 = new ZCandle(candle2);
-    expect(zcandle2.toString()).to.equal(zippedCandleStr2);
-    const zcandle3 = new ZCandle(candle3);
-    expect(zcandle3.toString()).to.equal(zippedCandleStr3);
-    const zcandle4 = new ZCandle(candle4);
-    expect(zcandle4.toString()).to.equal(zippedCandleStr4);
-    const zcandle5 = new ZCandle(candle5);
-    expect(zcandle5.toString()).to.equal(zippedCandleStr5);
-    const zcandle6 = new ZCandle(candle6);
-    expect(zcandle6.toString()).to.equal(zippedCandleStr6);
-  });
-  it('should init from zipped', function () {
-    const zcandle1 = new ZCandle(zippedCandleStr1);
-    expect(zcandle1.toString()).to.equal(zippedCandleStr1);
+describe('ZCandle', function suite() {
+    const zippedCandleStr1 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100';
+    const zippedCandleStr2 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::42';
+    const zippedCandleStr3 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::-151500::42';
+    const zippedCandleStr4 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-151500::42';
+    const zippedCandleStr5 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-::42';
+    const zippedCandleStr6 = 'C::KRAKEN::BTCUSD::1d::2020-08-02T00:00:00.000Z::10000::10111::10000::10100::15-';
+    const opts = {
+        exchange: 'KRAKEN',
+        symbol: 'BTCUSD',
+        interval: '1d',
+        timestamp: '2020-08-02T00:00:00.000Z',
+        open: '10000',
+        high: '10111',
+        low: '10000',
+        close: '10100',
+    };
+    const candle1 = new Candle(opts);
+    const candle2 = new Candle({...opts, trades: "42"});
+    const candle3 = new Candle({...opts, trades: "42", volume: {quote: "151500"}});
+    const candle4 = new Candle({...opts, trades: "42", volume: {quote: "151500", base: "15"}});
+    const candle5 = new Candle({...opts, trades: "42", volume: {base: "15"}});
+    const candle6 = new Candle({...opts, volume: {base: "15"}});
+    it('should instantiates', () => {
+        zcandle = new ZCandle();
+        expect(zcandle).to.exist;
+    })
+    it('should init from candle', function () {
+        const zcandle1 = new ZCandle(candle1);
+        expect(zcandle1.toString()).to.equal(zippedCandleStr1);
+        const zcandle2 = new ZCandle(candle2);
+        expect(zcandle2.toString()).to.equal(zippedCandleStr2);
+        const zcandle3 = new ZCandle(candle3);
+        expect(zcandle3.toString()).to.equal(zippedCandleStr3);
+        const zcandle4 = new ZCandle(candle4);
+        expect(zcandle4.toString()).to.equal(zippedCandleStr4);
+        const zcandle5 = new ZCandle(candle5);
+        expect(zcandle5.toString()).to.equal(zippedCandleStr5);
+        const zcandle6 = new ZCandle(candle6);
+        expect(zcandle6.toString()).to.equal(zippedCandleStr6);
+    });
+    it('should init from zipped', function () {
+        const zcandle1 = new ZCandle(zippedCandleStr1);
+        expect(zcandle1.toString()).to.equal(zippedCandleStr1);
 
-    const zcandle2 = new ZCandle(zippedCandleStr2);
-    expect(zcandle2.toString()).to.equal(zippedCandleStr2);
+        const zcandle2 = new ZCandle(zippedCandleStr2);
+        expect(zcandle2.toString()).to.equal(zippedCandleStr2);
 
-    const zcandle3 = new ZCandle(zippedCandleStr3);
-    expect(zcandle3.toString()).to.equal(zippedCandleStr3);
+        const zcandle3 = new ZCandle(zippedCandleStr3);
+        expect(zcandle3.toString()).to.equal(zippedCandleStr3);
 
-    const zcandle4 = new ZCandle(zippedCandleStr4);
-    expect(zcandle4.toString()).to.equal(zippedCandleStr4);
+        const zcandle4 = new ZCandle(zippedCandleStr4);
+        expect(zcandle4.toString()).to.equal(zippedCandleStr4);
 
-    const zcandle5 = new ZCandle(zippedCandleStr5);
-    expect(zcandle5.toString()).to.equal(zippedCandleStr5);
+        const zcandle5 = new ZCandle(zippedCandleStr5);
+        expect(zcandle5.toString()).to.equal(zippedCandleStr5);
 
-    const zcandle6 = new ZCandle(zippedCandleStr6);
-    expect(zcandle6.toString()).to.equal(zippedCandleStr6);
-  });
-  it('should convert to candle', function () {
-    const zcandle1 = new ZCandle(zippedCandleStr1);
-    expect(zcandle1.toCandle()).to.deep.equal(candle1);
+        const zcandle6 = new ZCandle(zippedCandleStr6);
+        expect(zcandle6.toString()).to.equal(zippedCandleStr6);
+    });
+    it('should convert to candle', function () {
+        const zcandle1 = new ZCandle(zippedCandleStr1);
+        expect(zcandle1.toCandle()).to.deep.equal(candle1);
 
-    const zcandle2 = new ZCandle(zippedCandleStr2);
-    expect(zcandle2.toCandle()).to.deep.equal(candle2);
+        const zcandle2 = new ZCandle(zippedCandleStr2);
+        expect(zcandle2.toCandle()).to.deep.equal(candle2);
 
-    const zcandle3 = new ZCandle(zippedCandleStr3);
-    expect(zcandle3.toCandle()).to.deep.equal(candle3);
+        const zcandle3 = new ZCandle(zippedCandleStr3);
+        expect(zcandle3.toCandle()).to.deep.equal(candle3);
 
-    const zcandle4 = new ZCandle(zippedCandleStr4);
-    expect(zcandle4.toCandle()).to.deep.equal(candle4);
+        const zcandle4 = new ZCandle(zippedCandleStr4);
+        expect(zcandle4.toCandle()).to.deep.equal(candle4);
 
-    const zcandle5 = new ZCandle(zippedCandleStr5);
-    expect(zcandle5.toCandle()).to.deep.equal(candle5);
+        const zcandle5 = new ZCandle(zippedCandleStr5);
+        expect(zcandle5.toCandle()).to.deep.equal(candle5);
 
-    const zcandle6 = new ZCandle(zippedCandleStr6);
-    expect(zcandle6.toCandle()).to.deep.equal(candle6);
-  });
-  it('should create from candle', function () {
-    const zcandle = new ZCandle(zippedCandleStr1);
-    const candle = zcandle.toCandle();
+        const zcandle6 = new ZCandle(zippedCandleStr6);
+        expect(zcandle6.toCandle()).to.deep.equal(candle6);
+    });
+    it('should create from candle', function () {
+        const zcandle = new ZCandle(zippedCandleStr1);
+        const candle = zcandle.toCandle();
 
-    expect(new ZCandle(candle)).to.deep.equal(zcandle);
-  });
-  it('should clone', function () {
-    const zcandle = new ZCandle(zippedCandleStr1);
-    const zcandle1 = new ZCandle(zcandle);
-    expect(zcandle1.clone()).to.deep.equal(zcandle);
-  });
+        expect(new ZCandle(candle)).to.deep.equal(zcandle);
+    });
+    it('should clone', function () {
+        const zcandle = new ZCandle(zippedCandleStr1);
+        const zcandle1 = new ZCandle(zcandle);
+        expect(zcandle1.clone()).to.deep.equal(zcandle);
+    });
+    it('should correctly go back and forth', function () {
+        const x = {
+            "market": {"exchange": "FTX", "type": "PERP", "symbol": "BTC-PERP", "quote": "USD", "base": "BTC"},
+          interval: '10s',
+          open: '19130',
+          close: '19130',
+          low: '19129',
+          high: '19130',
+          volume: { base:'0.0044', quote: '84.1717' },
+          openTime: { date: '2022-10-15T18:27:00.000Z' },
+          closeTime:  { date: '2022-10-15T18:27:09.999Z' },
+          trades: '4'
+        };
+        const candle = new Candle(x);
+        const zcandle = candle.toZCandle();
+        expect(zcandle.toCandle()).to.deep.equal(candle);
+    });
 });
