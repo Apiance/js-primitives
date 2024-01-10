e2de3ff93122edcfeb9b2ed98f86e6fc938a9413
Alex Werner <obusco@gmail.com>
2024-01-10 09:32:16 +0100

    fix(Trade): tradeId util export

--- src/Trade/utils/computeTradeId.js
+++ src/Trade/utils/computeTradeId.js
@@ -3,4 +3,5 @@ function computeTradeId(trade){
     const hash = 'T'+(crypto.createHash('sha1').update(trade.toCompressed(), 'utf-8').digest('hex').slice(0,8)).toUpperCase();
     return hash;
 };
-module.exports = computeTradeId
+
+export default computeTradeId;
